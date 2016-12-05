e2c3ef0b40cd119033c0db4d135b2fa663c2fc65
ntuchol <ntuchol@bu.edu>
2016-12-05 13:28:18 -0600

    Add files via upload

--- MyProgram.java
+++ MyProgram.java
@@ -1,7 +1,13 @@
 public class MyProgram {
  public static void main(String[] args) {
-     System.out.println("\"Go!\", the crowd shouted. \"Go Terriers!\"");
-     System.out.println("/\\ //\\\\ /\\");
-     System.out.println("Later they chanted, \"We're going to win!\"");
+     Woo w = new Woo();
+   System.out.println(w.one() + " " + w.two() + " " + w.extra());
+   Woo y1 = new Yoo();
+   System.out.println(y1.one() + " " + y1.two() + " " + y1.extra()); 
+   Yoo y2 = new Yoo();
+   System.out.println(y2.one() + " " + y2.two() + " " + y2.extra()); 
+   Zoo z = new Yoo();
+   System.out.println(z.one() + " " + z.two() + " " + z.extra()); 
+   
  }
 }
