836a71c043122d854bcabe1a2ae910f057f15024
ntuchol <ntuchol@bu.edu>
2016-12-05 13:37:41 -0600

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -3,168 +3,21 @@
  * 
  * Computer Science 111, Boston University
  * 
- * A class that acts as a blueprint for objects that represent a rectangle.
- * This class is comparable to the built-in Rectangle class
- * from the java.awt package.
+ * A rudimentary initial version of a class for Rectangle objects.
+ * These objects simply group together four variables.
+ * 
+ * Given this version of the class, our client program 
+ * has to do most of the work itself.
+ * 
+ * In the lecture notes, we gradually add features to this class that
+ * simplify the client program and provide encapsulation.  
+ * 
+ * See the rectangle_final directory for the final version of the class.
  */
 
 public class Rectangle {
-    /* 
-     * Fields that capture the state of a Rectangle object.
-     * We make them private to prevent direct access from outside the class.
-     */
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-
-    /* 
-     * A constructor that allows clients to specify initial values for all
-     * of the fields.  It calls the mutator methods, so that they
-     * can perform the necessary error-checking.
-     */
-    public Rectangle(int initialX, int initialY,
-      int initialWidth, int initialHeight) {
-        this.setLocation(initialX, initialY);
-        this.setWidth(initialWidth);
-        this.setHeight(initialHeight);
-    }
-
-    /* 
-     * A constructor that takes just the width and height, 
-     * and assumes that the coordinates are (0, 0).
-     * It calls the other constructor to do the work.
-     */
-    public Rectangle(int initialWidth, int initialHeight) {
-        this(0, 0, initialWidth, initialHeight);
-    }
-    
-    /*
-     * getX - an accessor method for a Rectangle's x coordinate.
-     */
-    public int getX() {
-        return this.x;
-    }
-    
-    /*
-     * getY - an accessor method for a Rectangle's y coordinate.
-     */
-    public int getY() {
-        return this.y;
-    }
-    
-    /* 
-     * getWidth - an accessor method for a Rectangle's width.
-     */
-    public int getWidth() {
-        return this.width;
-    }
-    
-    /*
-     * getHeight - an accessor method for a Rectangle's height.
-     */
-    public int getHeight() {
-        return this.height;
-    }         
-
-    /*
-     * area - an accessor method for a Rectangle's area.
-     */
-    public int area() {
-        return this.width * this.height;
-    }
-    
-    /*
-     * perimeter - an accessor method for a Rectangle's perimeter.
-     */
-    public int perimeter() {
-        return (2*this.width + 2*this.height);
-    }
-    
-    /*
-     * setLocation - a mutator method that changes a Rectangle's 
-     * x and y coordinates.
-     *
-     * precondition: both newX and newY must be non-negative
-     */
-    public void setLocation(int newX, int newY) {
-        if (newX < 0 || newY < 0) {
-            throw new IllegalArgumentException();
-        }
-
-        this.x = newX;
-        this.y = newY;
-    }
-    
-    /*
-     * setWidth - a mutator method that changes a Rectangle's width.
-     *
-     * precondition: newWidth must be positive
-     */
-    public void setWidth(int newWidth) {
-        if (newWidth <= 0) {
-            throw new IllegalArgumentException();
-        }
-
-        this.width = newWidth;
-    }
-
-    /*
-     * setHeight - a mutator method that changes a Rectangle's height.
-     *
-     * precondition: newHeight must be positive
-     */
-    public void setHeight(int newHeight) {
-        if (newHeight <= 0) {
-            throw new IllegalArgumentException();
-        }
-        
-        this.height = newHeight;
-    }
-    
-    /*
-     * grow - a mutator method that changes a Rectangle's width and height,
-     * adding dWidth to the current width and dHeight to the current height.
-     * 
-     * It uses setWidth and setHeight so that they can perform the
-     * necessary error-checking.
-     *
-     * precondition: the resulting width and height must be positive.
-     */
-    public void grow(int dWidth, int dHeight) {
-        this.setWidth(this.width + dWidth);
-        this.setHeight(this.height + dHeight);
-    }
-    
-    /*
-     * isSquare - returns true if the Rectangle is a square, and false otherwise.
-     */
-    public boolean isSquare() {
-        return (this.height == this.width);
-        
-        
-    }
-    
-    /*
-     * equals - returns true if the Rectangle specified by the parameter
-     * is equivalent to the calling object, and false otherwise.
-     * 
-     * Note: this method does *not* override the default equals method,
-     * because its parameter is of type Rectangle.
-     */
-    public boolean equals(Rectangle other) {
-        return (other != null           
-             && this.x == other.x 
-             && this.y == other.y 
-             && this.width == other.width 
-             && this.height == other.height);
-    }
-   
-    /*
-     * toString - returns a string representation of the Rectangle 
-     * of the form "width x height".
-     */
-     public String toString() {
-         return this.width + " x " + this.height;
-     }
+    int x;
+    int y;
+    int width;
+    int height;
 }
--- RectangleClient.java
+++ RectangleClient.java
@@ -1,26 +1,39 @@
 /*
  * RectangleClient.java
  * 
- * A client program of our Rectangle class.
+ * A client program for our rudimentary Rectangle class.
+ * This version of the program has to do most of the work itself.
  * 
- * This file must be put in the same directory as Rectangle.java.
+ * See the rectangle_final directory for the final version
+ * of the client program.
  */
+
 public class RectangleClient {
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle(10, 20, 100, 50);
-        Rectangle r2 = new Rectangle(50, 100, 20, 80);
+        Rectangle r1 = new Rectangle();
+        r1.x = 10;       r1.y = 20;
+        r1.width = 100;  r1.height = 50;
+        
+        Rectangle r2 = new Rectangle();
+        r2.x = 50;       r2.y = 100;
+        r2.width = 20;   r2.height = 80;
+        
+        System.out.println("r1: " + r1.width + " x " + r1.height);
+        int area1 = r1.width * r1.height;
+        System.out.println("area = " + area1);
+        int perimeter1 = 2*r1.width + 2*r1.height;
+        System.out.println("perimeter = " + perimeter1);
+        System.out.println();
 
-        System.out.println("r1: " + r1.getWidth() + " x " + r1.getHeight());
-        System.out.println("area = " + r1.area());
-        System.out.println("perimeter = " + r1.perimeter());
+        System.out.println("r2: " + r2.width + " x " + r2.height);
+        int area2 = r2.width * r2.height;
+        System.out.println("area = " + area2);
+        int perimeter2 = 2*r2.width + 2*r2.height;
+        System.out.println("perimeter = " + perimeter2);
         System.out.println();
-        
-        System.out.println("r2: " + r2);   // the toString method will be called
-        System.out.println("area = " + r2.area());
-        System.out.println("perimeter = " + r2.perimeter());
-        System.out.println();
-        
-        if (r1.equals(r2)) {
+
+        if (r1.x == r2.x && r1.y == r2.y 
+          && r1.width == r2.width && r1.height == r2.height) {
             System.out.println("The two rectangles are equivalent.");
         } else {
             System.out.println("The two rectangles are not equivalent.");
@@ -28,10 +41,12 @@ public class RectangleClient {
         System.out.println();
         
         // grow both rectangles
-        r1.grow(50, 10);
-        r2.grow(5, 30);
-        
-        System.out.println("r1: " + r1);
-        System.out.println("r2: " + r2);
+        r1.width += 50;
+        r1.height += 10;
+        r2.width += 5;
+        r2.height += 30;
+
+        System.out.println("r1: " + r1.width + " x " + r1.height);
+        System.out.println("r2: " + r2.width + " x " + r2.height);
     }
 }
