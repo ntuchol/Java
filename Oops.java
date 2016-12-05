54bb7de2dc555271ea03eb82d84219b5a82b981c
ntuchol <ntuchol@bu.edu>
2016-12-05 13:36:47 -0600

    Add files via upload

---
+++ Oops.java
@@ -0,0 +1,11 @@
+ public class Oops {
+           public static void main(String[] args) {
+               double x = 0.0;
+               System.out.println("x is " + x);        // print: x is 0.0
+               x = 15.2;                        // set x to 15.2
+               System.out.println("x is now " + x);
+               double y;                               
+               y = x + 1;                       // set y to 1 more than x
+               System.out.println("x and y are " + x + " and " + y); 
+           } 
+       }
