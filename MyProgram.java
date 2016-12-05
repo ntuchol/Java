d443626113c4bba9f525695b1227af52953c9309
ntuchol <ntuchol@bu.edu>
2016-12-05 13:19:24 -0600

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,7 @@
+public class MyProgram {
+ public static void main(String[] args) {
+     System.out.println("\"Go!\", the crowd shouted. \"Go Terriers!\"");
+     System.out.println("/\\ //\\\\ /\\");
+     System.out.println("Later they chanted, \"We're going to win!\"");
+ }
+}
